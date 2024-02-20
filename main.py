508b1321bbebafbe2351ff1c45a6430302204af8
Tejas Rao <tejasr070@gmail.com>
2024-02-20 21:24:23 +0000

    Created branch for tejj

--- main.py
+++ main.py
@@ -8,11 +8,6 @@ clientSecret = "c25cccf74fa54c579838da61cc581728"
 headers = {
     
 }
-
-params = {
-    
-}
-
 r = requests.post(url=authURL, headers=headers, params=params)
 
 def getToken(url : str, clientID : str, clientKey : str) -> str :
