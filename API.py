56cebc6cba6bd92843bb2629db5e71ee02605e8f
Tejas Rao <tejasr070@gmail.com>
2024-03-03 06:24:10 +0000

    update API.py

--- API.py
+++ API.py
@@ -13,6 +13,8 @@ class SpotifyAPI:
         self.base_URL = "https://api.spotify.com/v1/"
         
     def get_token(self) -> str:
+        '''POST request to auth server to obtain client token for data requests
+        '''
         authString = f"{self.clientID}:{self.clientSecret}"
         authBytes = authString.encode("utf-8")
         authBase64 = str(base64.b64encode(authBytes), "utf-8")
@@ -30,21 +32,25 @@ class SpotifyAPI:
         return json_result["access_token"]
     
     def generate_auth_header(self, token: str) -> dict:
+        '''Generate a header in the required format for spotify API
+        '''
         return {"Authorization": "Bearer " + token}
     
-    def get_data(self, token: str, search_type: str, search_param: str = None) -> dict:
-        query_URL = self.base_URL + search_type + '/' + search_param
+    def get_data(self, token: str, search_type: str, search_param: str = None, batch = False) -> dict:
+        '''token : str -> auth token provided by authClient
+        search_type : str -> type of resource we are querying - audio-features/tracks/playlists/artists/albums
+        search_param : str -> resource URI
+        batch : bool -> if true then send bulk request with multiple comma-separated URIs 
+        '''
+        if(batch) :
+            query_URL = self.base_URL + search_type + '?' + search_param
+        else :
+            query_URL = self.base_URL + search_type + '/' + search_param
         headers = self.generate_auth_header(token)
         
         response = requests.get(url=query_URL, headers=headers)
         json_result = response.json()
-        print(f"Status Code: {response.status_code}")
+        # print(f"Status Code: {response.status_code}")
         
         return json_result
 
-
-spotify = SpotifyAPI()
-token = spotify.get_token()
-print(f"Token: {token}")
-result = spotify.get_data(token, "tracks", "4cxMGhkinTocPSVVKWIw0d")
-print(result)
