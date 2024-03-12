0db3514a95de89af29ff9895c7052ad058ed04a0
Tejas Rao <tejasr070@gmail.com>
2024-03-12 23:15:34 +0000

    Added genre information

--- enrich.py
+++ enrich.py
@@ -1,6 +1,7 @@
 from API import SpotifyAPI
 import requests
 import networkx as nx
+from collections import Counter
 
 
 def batch(nodes : list, batch_size : int) -> list :
@@ -13,14 +14,32 @@ def batch(nodes : list, batch_size : int) -> list :
         batches.append(','.join(nodes[i : i + batch_size]))
     return batches
 
-def node_batch(G : nx.graph, batch_size : int) -> list :
+def node_batch(G : nx.graph, batch_size : int, type : str = "tracks") -> list :
     '''Takes a graph as input, extracts object ID from URIs and creates batches
     to be used for batch API query. Batches are comma separated object IDs
     G : graph -> Input graph
+    type : str -> tracks/artist/albums/playlists -> creates batch based on object type
+    type = "{object}" will create a batch based on {object} URIs 
     '''
     nodeList = []
-    for node in G.nodes(data = False) :
-        nodeList.append(node.split(":")[-1])
+    if(type == "tracks") :
+        for node in G.nodes(data = False) :
+            nodeList.append(node.split(":")[-1])
+    elif(type == "artists") :
+        for node, attrs in G.nodes(data = True) :
+            for attr, val in attrs.items() :
+                if(attr == "Artist URI") :
+                    nodeList.append(val.split(":")[-1])
+    elif(type == "albums") :
+        for node, attrs in G.nodes(data = True) :
+            for attr, val in attrs.items() :
+                if(attr == "Album URI") :
+                    nodeList.append(val.split(":")[-1])
+    elif(type == "playlists") :
+        for node, attrs in G.nodes(data = True) :
+            for attr, val in attrs.items() :
+                if(attr == "Playlist URI") :
+                    nodeList.append(val.split(":")[-1])
         
     trackBatch = batch(nodeList, batch_size)
     
@@ -121,25 +140,58 @@ def get_artist_details(batches : list, api_object, token : str) -> dict :
         try:
             query = "ids=" + batch
             data = api_object.get_data(token, 'artists', query, batch=True)
-            print(f"len data : {len(data['artists'])}")
             features.append(data)
         except Exception as err:
             print(f"An unexpected error occurred: {err}")  # Handle other unexpected errors
-    print(f"artist detail : {features[0]["artists"]}")
+
     return features
 
-def update_artist_details(G : nx.Graph, artist_details : dict) -> None :
+def update_artist_details(G : nx.Graph, artist_details : dict, top_genres : list) -> None :
     artist_details_dict = {}
     for artists in artist_details :
         for artist in artists['artists'] :
             new_dict = {
                 artist["uri"]: {
                     "artist_popularity": artist["popularity"],
-                    "genre": artist["genre"]
+                    "genres": artist["genres"]
                 }
             }
             artist_details_dict.update(new_dict)
-            
+    
+    for node, attrs in G.nodes(data=True) :
+        for artist_uri in list(artist_details_dict) :
+            if(artist_uri == G.nodes[node]["Artist URI"]) :
+                cleanedGenres = clean_genres(artist_details_dict[artist_uri]["genres"], top_genres)
+                artist_details_dict[node] = artist_details_dict[artist_uri]
+                artist_details_dict[node]["genres"] = list(cleanedGenres)
+                
     nx.set_node_attributes(G, artist_details_dict)
     
     return
+
+def clean_genres(track_genres : list, all_genres : list) -> list :
+    cleanedGenre = set()
+    for track_genre in track_genres :
+        for genre in all_genres :
+            if(genre.lower() in track_genre.lower()) :
+                cleanedGenre.add(genre)
+                
+    return cleanedGenre
+
+def get_all_genres(artist_details : list) -> dict :
+    genres_list = []
+    allowed_genres = ['Hip Hop', 'Pop', 'Rock', 'Country', 'Rap', 'EDM', 'Indie', 'R&B', 'Trap', 'Electro', 'Mellow']
+    for artists in artist_details :
+        for artist in artists['artists'] :
+            genres_list.extend(artist['genres'])
+
+    for i in range(0, len(genres_list)) :
+        for genre in allowed_genres :
+            if(genre.lower() in genres_list[i].lower()) :
+                genres_list[i] = genre
+    
+    all_genres = Counter(genres_list)
+    
+        
+    return all_genres
+         
--- main.py
+++ main.py
@@ -1,16 +1,18 @@
+# %%
 import json
 from API import SpotifyAPI
 from NetworkAnalysis import NetworkAnalysis
 from enrich import *
 import networkx as nx
 
-
+# %%
 spotify = SpotifyAPI()
 token = spotify.get_token()
 print(f"Token: {token}")
 # result = spotify.get_data(token, "tracks", "4cxMGhkinTocPSVVKWIw0d")
 # print(result)
 
+# %%
 small_graph = NetworkAnalysis(r".\Small Dataset\small_graph.pickle", r".\Small Dataset\song_data_smaller.csv")
 small_graph.get_connected_components()
 small_graph.plot_centrality_measures()
@@ -18,25 +20,35 @@ small_graph.eccentricity()
 small_graph.pageRank(alpha=0.85, tol=1e-4, max_iter=500)
 eccentricity = diameter = small_graph.diameter()
 print(f"eccentricity : {eccentricity}")
-gn_communities = small_graph.girvan_newman(5)
+# gn_communities = small_graph.girvan_newman(5)
 # print(f"Girvan - Newman : {gn_communities}")
 
+# %%
 # Enrich track nodes with API
 #Split the nodes into batches for faster querying rather than querying each URI
-nodeBatches = node_batch(small_graph.G, 50)
+nodeBatches = node_batch(small_graph.G, 50, type = "tracks")
 
 #Get track details and update node attributes
 track_details = get_track_details(nodeBatches, spotify, token)
 update_track_attributes(small_graph.G, track_details)
 
+# %%
 #Get track audio features and update node attributes
 audio_features = get_audio_features(nodeBatches, spotify, token)
 update_audio_attributes(small_graph.G, audio_features)
 
+# %%
 # Get track  and update node attributes
-artist_details = get_artist_details(nodeBatches, spotify, token)
-update_artist_details(small_graph.G, artist_details)
+artistBatches = node_batch(small_graph.G, 50, type = "artists")
+artist_details = get_artist_details(artistBatches, spotify, token)
 
+all_genres = get_all_genres(artist_details)
+top_genres = top_20_keys = {key : value for key, value in sorted(all_genres.items(), key=lambda item: item[1], reverse=True)[:10]}
+update_artist_details(small_graph.G, artist_details, list(top_genres))
+
+# print(f"All Genres : {top_genres}")
+# # %%
 small_graph.display_nodes()
 
 small_graph.save_graph()
+# # %%
